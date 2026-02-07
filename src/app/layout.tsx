e4b169101141512d7a36fa251986a13a2f2f7959
ravindra <sirviravindra609@gmail.com>
2026-02-08 02:17:23 +0530

    feat: Integrate Vercel Analytics for usage tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import WhatsAppButton from "@/components/WhatsAppButton";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 {children}
                 <WhatsAppButton />
+                <Analytics />
             </body>
         </html>
     );
